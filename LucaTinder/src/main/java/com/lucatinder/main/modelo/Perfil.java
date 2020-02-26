d3bb77aec44133208cdc4a60ae61fa56c6d62ae0
German Sanchez <luqh555@gmail.com>
2020-02-26 16:55:24 +0100

    crear com.lucatinder.main.modelo Perfil
     Se crea el perfil con los join, especial cuidado al crear clase
    Materia.

---
+++ LucaTinder/src/main/java/com/lucatinder/main/modelo/Perfil.java
@@ -0,0 +1,105 @@
+package com.lucatinder.main.modelo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "persona")
+
+public class Perfil {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idPerfil;
+	private String nombre;
+	private int genero;
+	private int edad;
+	private String descripcion;
+	private String descripcionCorta;
+	private String poblacion;
+	private String foto;
+	
+	@JoinTable
+	(name = "intereses", 
+	 joinColumns = { 
+			 @JoinColumn(
+					name = "idPerfil", 
+					referencedColumnName = "idPerfil") }, 
+	 inverseJoinColumns = { 
+			 @JoinColumn(
+			 		name = "idMateria",
+			 		referencedColumnName = "idMateria") })
+
+@ManyToMany(
+		fetch = FetchType.LAZY, 
+		cascade = CascadeType.ALL)
+	
+	
+	private ArrayList<Materia> arrayMateria;
+	public int getIdPerfil() {
+		return idPerfil;
+	}
+	public void setIdPerfil(int idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getGenero() {
+		return genero;
+	}
+	public void setGenero(int genero) {
+		this.genero = genero;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getDescripcionCorta() {
+		return descripcionCorta;
+	}
+	public void setDescripcionCorta(String descripcionCorta) {
+		this.descripcionCorta = descripcionCorta;
+	}
+	public String getPoblacion() {
+		return poblacion;
+	}
+	public void setPoblacion(String poblacion) {
+		this.poblacion = poblacion;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public ArrayList<Materia> getArrayMaterias() {
+		return arrayMaterias;
+	}
+	public void setArrayMaterias(ArrayList<Materia> arrayMaterias) {
+		this.arrayMaterias = arrayMaterias;
+	}
+	
+	
+	
+}
