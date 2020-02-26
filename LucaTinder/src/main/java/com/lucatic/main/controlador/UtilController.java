e0f2095842be4f33badfc61755e17eec0149f797
Edmund Home <edmundhome@hotmail.com>
2020-02-26 17:28:30 +0100

    Crear clase CargarDatos,UtilController y implementar la libreria faker

---
+++ LucaTinder/src/main/java/com/lucatic/main/controlador/UtilController.java
@@ -0,0 +1,9 @@
+package com.lucatic.main.controlador;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("util")
+public class UtilController {
+	
+	
+}
---
+++ LucaTinder/src/main/java/com/lucatic/main/utilidades/CargarDatos.java
@@ -0,0 +1,20 @@
+package com.lucatic.main.utilidades;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CargarDatos {
+	
+	public void cargarDatos() {
+		
+		
+	}
+	
+	public ArrayList<Object> generarPerfiles(){
+		
+		return null;
+	}
+
+}
