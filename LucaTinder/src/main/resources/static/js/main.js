c97ac717f32502d71fd5a283d75be7b9859dff02
Eduardo Arroyo <eduardoarroyo1987@gmail.com>
2020-03-04 22:10:14 +0100

    Creacion de edit html y js para la carga de la foto

--- LucaTinder/src/main/resources/static/js/main.js
+++ LucaTinder/src/main/resources/static/js/main.js
@@ -1,7 +1,15 @@
-<script>
-$('.openBtn').on('click',function(){
-    $('.modal-body').load('content.html',function(){
-        $('#myModal').modal({show:true});
-    });
-});
-</script>
+document.getElementById("foto").onchange = function(e) {
+	let reader = new FileReader();
+  
+  reader.onload = function(){
+    let preview = document.getElementById('preview'),
+    		image = document.createElement('img');
+
+    image.src = reader.result;
+    
+    preview.innerHTML = '';
+    preview.append(image);
+  };
+ 
+  reader.readAsDataURL(e.target.files[0]);
+}
